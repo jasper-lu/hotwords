a3c8bc41ece49b9df5b35fd69b379876e1ef978b
Jasper Lu <repsajrules@gmail.com>
2013-10-12 17:26:03 -0400

    backgorund and more

--- hotwords.js
+++ hotwords.js
@@ -46,44 +46,111 @@ var words = [
 var iter = 0;
 var teams = 2;
 var tick = false;
-var timer = teams * 5;
-
+var timer;
+var beepBeep = [false, false, false];
 var change = function (id) {
     $('.current').toggleClass('current');
     $('#' + id).toggleClass('current');
 };
+var playerNumb = 2;
 
-$(document).ready(function () {
-    
-    //events
-    $('#play').click(function () {
-        words.sort(function () { return Math.random() - 0.5 });
-        change('green');
-    });
-
-    $('#green').mousedown(function () {
+document.addEventListener('touchstart', function (e) {
+    var id = $('.current').attr('id');
+    if (id == 'green') {
         tick = true;
         $('.word').html(words[iter]);
         change('word');
+    } else if (id == 'reveal') {
+        tick = true;
+        ++iter;
+        $('.word').html(words[iter]);
+        change('word');
+    }
+});
+document.addEventListener('touchend', function (e) {
+    var id = $('.current').attr('id');
+    if (id == 'word') {
+        change('progress');
+    }
+})
+
+
+$('#word').click(function () {
+    change('progress');
+});
+$(document).ready(function () {
+
+    //sound initialization
+    var beep = document.createElement('audio');
+    beep.setAttribute('src', 'beep.mp3');
+    var alarm = document.createElement('audio');
+    alarm.setAttribute('src', 'alarm.mp3');
+    $.get();
+
+    //events
+    $('#up').click(function () {
+        playerNumb++;
+        if (playerNumb >= 4) {
+            playerNumb = 4;
+            $('#up').css('color', 'grey'); $('#playernumb').html(playerNumb + '');
+        } else {
+            $('#playernumb').html(playerNumb + '');
+            $('#down').css('color', 'green');
+        }
     });
 
-    $('#word').mouseup(function () {
-        change('progress');
+    $('#down').click(function () {
+        playerNumb--;
+        if (playerNumb <= 2) {
+            playerNumb = 2;
+            $('#down').css('color', 'grey'); $('#playernumb').html(playerNumb + '');
+        } else {
+            $('#playernumb').html(playerNumb + '');
+            $('#up').css('color', 'green');
+        }
     });
+
+    $('#play').click(function () {
+        change('players');
+    });
+
+    $('#start').click(function () {
+        words.sort(function () { return Math.random() - 0.5 });
+        timer = teams * 5;
+        change('green');
+    })
+    //beep beeps
+    
+    setInterval(function () {
+        if (tick) {
+            beep.play();
+            $('#progress').toggleClass('blanca');
+            setTimeout('$("#progress").toggleClass("blanca")', 100);
+        }
+    }, 500);
+    /*
+    setInterval(function () {
+        if (tick && beepBeep[1])
+            beep.play();
+    }, 1000);
+    setInterval(function () {
+        if (tick && beepBeep[2])
+            beep.play();
+    }, 1500);
+
+    setInterval(function () {
+        beepBeep = [false, false, false];
+        beepBeep[Math.floor(Math.random() * 3)] = true;
+    }, 5000);
+    */
 
     $('#progress').click(function () {
         tick = false;
         change('reveal');
     });
 
-    $('#reveal').mousedown(function () {
-        ++iter;
-        $('.word').html(words[iter]);
-        tick = true;
-        change('word');
-    });
-
     $('#end').click(function () {
+        tick = false;
         change('main');
     });
 
@@ -92,9 +159,13 @@ $(document).ready(function () {
     //timer
 
     setInterval(function () {
-        if (tick)
+        if (tick) {
             timer--;
-        if (timer == 0)
-            change('end');
+            if (timer == 0) {
+                change('end');
+                tick = false;
+                alarm.play();
+            }
+        }
     }, 1000);
 });
